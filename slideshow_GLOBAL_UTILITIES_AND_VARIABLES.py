22b2120c2b38fc7d4326687d95ca92e10b51bc19
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-06-24 15:49:24 +0930

    Add files via upload

--- slideshow_GLOBAL_UTILITIES_AND_VARIABLES.py
+++ slideshow_GLOBAL_UTILITIES_AND_VARIABLES.py
@@ -1,3 +1,18 @@
+# Copyright (C) <2023> <ongoing>  <hydra3333>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # We could use this module slideshow_GLOBAL_UTILITIES_AND_VARIABLES to store global variables.
 # EVERYONE (EXCEPT ITSELF) must import slideshow_GLOBAL_UTILITIES_AND_VARIABLES 
 # Then global variables can be stored and shared acorss modules
--- slideshow_LOAD_SETTINGS.py
+++ slideshow_LOAD_SETTINGS.py
@@ -1,3 +1,18 @@
+# Copyright (C) <2023> <ongoing>  <hydra3333>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # 1.	Modifying the sys.path list in a module DOES NOT not affect the sys.path of other modules or the main program.
 # 2.	Modifying the sys.path list in the MAIN PROGRAM WILL affect the search path for	all modules imported by that program.
 # Ensure we can import modules from ".\" by adding the current default folder to the python path.
